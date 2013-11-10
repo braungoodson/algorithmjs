de75976ec2ba734d6f439afb15b8573328ef94dd
Infinite Flux Master <braungoodson@gmail.com>
2013-11-10 13:38:25 -0500

    + refactor

--- find-max-crossing-subarray.js
+++ find-max-crossing-subarray.js
@@ -1,12 +1,5 @@
 var negativeInfinity = -65000;
-var a = [13,-3,-25,20,-3,-16,-23,18,20,-7,12,-5,-22,15,-4,7];
-var l = 0;
-var m = Math.floor((a.length-1)/2);
-var h = a.length-1;
-console.log(
-  findMaxCrossingSubarray(a,l,m,h)
-);
-// returns indexes into the array
+// returns tuple of indexes into the array
 function findMaxCrossingSubarray (A,low,mid,high) {
   var leftSum = negativeInfinity;
   var sum = 0;

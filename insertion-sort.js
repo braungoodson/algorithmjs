5b08c5cf73f3bd048678b6041288c611d53afee1
Infinite Flux Master <braungoodson@gmail.com>
2013-11-10 13:41:28 -0500

    + refactor

--- insertion-sort.js
+++ insertion-sort.js
@@ -1,10 +1,7 @@
-var input = [4,7,5,2,3,3,3,7,0,0,0,0,];
-insertionSort(input);
-console.log(input)
-function insertionSort (A) { console.log(A)
-  for (var j = 1; j < A.length; ++j) { console.log(j,A.length)
-    var key = A[j]; console.log(key,A[j])
-    var i = j - 1; console.log(i);
+function insertionSort (A) {
+  for (var j = 1; j < A.length; ++j) {
+    var key = A[j];
+    var i = j - 1;
     while ((i >= 0) && (A[i] > key)) {
       A[i + 1] = A[i];
       i = i - 1;

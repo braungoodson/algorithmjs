fa1f742c185135beccf115309c6b8238454a20db
braun <braun@brauns-iMac.local>
2013-11-10 12:20:51 -0500

    + OK

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
 unit-test:
 
-	@./node_modules/.bin/mocha
+	@./node_modules/.bin/mocha \
+	-w \
+	insertion-sort.js \
+	test/unit/insertion-sort.suite.js
 
 test:
 
---
+++ test/unit/insertion-sort.suite.js
@@ -0,0 +1,6 @@
+var expect = require('expect.js');
+describe('insertion-sort',function(){
+	it('should exist',function(){
+		expect(insertionSort).toBeDefined();
+	});
+});
